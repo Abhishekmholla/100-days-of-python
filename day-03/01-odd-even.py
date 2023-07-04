d604964b4ed2aa70923d9938748ca9d91346fdd2
aman0103 <aman0103@student.monash.edu>
2023-07-04 17:16:49 +1000

    Completed odd-even exercise in day03

---
+++ day-03/01-odd-even.py
@@ -0,0 +1,8 @@
+print("Welcome to odd-even check tool")
+
+number = int(input("Please enter a valid number to check? "))
+
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
