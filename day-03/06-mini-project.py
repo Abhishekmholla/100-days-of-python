69afabde5b4a261a09e2b0a9ad059ed51ade260c
aman0103 <aman0103@student.monash.edu>
2023-07-04 19:38:35 +1000

    Modified statements in  mini-project in day03

--- day-03/06-mini-project.py
+++ day-03/06-mini-project.py
@@ -1,22 +1,22 @@
-print("Welcome to Treasure Island\n" +
-      "Your mission is to find the treasure")
+print('''Welcome to Treasure Island\nYour mission is to find the treasure\n''')
 
-turn_choice = input("Do you want to turn right or left. Type r for right and l for left? ").lower()
+turn_choice = input('''You're at a crossroad!!\nDo you want to turn right or left. 
+                    \nType 'r' for right and 'l' for left?''').lower()
 
 if turn_choice != "l":
-    print("Game over")
+    print("You fell into a hole!! Game over")
     exit()
 
 swim_choice = input("Do you want to swim or wait. Type s for swim and w for wait? ").lower()
 
 if swim_choice != "w":
-    print("Game over")
+    print("There are crocodiles!!! Game over")
     exit()
 
 door_choice = input("Which door do you want to open?. Type r for red and y for yellow and b for blue? ").lower()
 
 if door_choice != "y":
-    print("Game over")
+    print("It is a room full of fire...Game over")
     exit()
 
 print("You win... The treasure is all yours!!!")
