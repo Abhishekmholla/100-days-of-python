96a422337e1ab9616b9bafe8d45851409d241673
aman0103 <aman0103@student.monash.edu>
2023-07-08 23:43:12 +1000

    Implemented concepts of range function in a loop

--- day-05/01-for-loop-usage.py
+++ day-05/01-for-loop-usage.py
@@ -4,3 +4,13 @@ fruits = ['Apple','Peach','Pear']
 for fruit in fruits:
     print(fruit)
     print(fruit + " Pie")
+
+# Prints number in the range 1 to 10
+print("Numbers in range 1-10")
+for number in range(1,11):
+    print(number)
+    
+print("Numbers in range 1-10 with step as 2")
+# Prints number in the range 1 to 10 with a step 2
+for number in range(1,11,2):
+    print(number)
