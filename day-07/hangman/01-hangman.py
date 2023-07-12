eeae472fe50d0c5909d08b670e799119f7ca306c
aman0103 <aman0103@student.monash.edu>
2023-07-12 15:40:14 +1000

    converted the hangman project to modules

--- day-07/hangman/01-hangman.py
+++ day-07/hangman/01-hangman.py
@@ -1,75 +1,17 @@
 import random
-
+import os
+from hangman_dependencies.hangman_art import logo,hangman_ascii
+from hangman_dependencies.hangman_words import word_list
 
 print("Welocome to the hangman game")
-print(''' 
- _                                             
-| |                                            
-| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
-| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
-| | | | (_| | | | | (_| | | | | | | (_| | | | |
-|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                    __/ |                      
-                   |___/''')
-
-hangman_ascii = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
+print(logo)
 
 # Constants
 lives = 6
 correct_word_count = 0
-random_words = ['ardvark','melbourne','london','camel','lion']
 
 # Choosing a word randomly
-word_selected = random.choice(random_words)
+word_selected = random.choice(word_list)
 print(
     f"The word is {len(word_selected)} letter long as shown below. Trying guessing it\n")
 print("_\t"*len(word_selected))
@@ -81,9 +23,15 @@ new_word = ("_\t"*len(word_selected)).strip('\t').split('\t')
 while lives != 0:
     user_input = input("\nGuess a letter: ").lower()
 
+    os.system('cls')
     # If the user input is invalid, throw error
     if len(user_input) == 0 or len(user_input) > 1:
         print("Please enter some valid letter to proceed with the game")
+        continue
+    
+    # If the user tries a letter already present in new word
+    if user_input in new_word:
+        print("You have already tried this letter. Please try a different letter")
         continue
     
     # If the user input cannot be found in the selected word, reduce one life and continue
@@ -92,6 +40,8 @@ while lives != 0:
             f"Bad choice!! I cannot see the letter in the word. You just lost one life and only {lives-1} lives remain")
         print(hangman_ascii[abs(lives - 7)])
         lives -= 1
+        if lives == 0:
+            print("Hard luck...You lose")
         continue
     
     print("That's right!!! I can see the letter in the word")
@@ -104,9 +54,9 @@ while lives != 0:
             new_word.insert(index, user_input)
 
     if len(word_selected) == correct_word_count:
-        print("Hard luck...You lose")
+        print("You win")
         lives = 0
 
     print("\n"+"\t".join(new_word))
-    print("\nYou win")
+
 
