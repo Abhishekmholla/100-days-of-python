6ad0ff7a45fd555c253ebc0223c67d6e3eb96d51
aman0103 <aman0103@student.monash.edu>
2023-07-13 00:04:50 +1000

    Basic functions with initialization created

--- day-08/01-basic-functions.py
+++ day-08/01-basic-functions.py
@@ -1,6 +1,14 @@
+# Simple function
 def greet():
     print("Hi")
     print("Hope you are doing well")
     
+# Function with a paramater which is initilization
+# Name is the parameter
+# Value passed is argument
+def greet_with_args(name = "default_name"):
+    print(f"Hi {name}")
 
 greet()
+greet_with_args("Abhishek")
+greet_with_args()
