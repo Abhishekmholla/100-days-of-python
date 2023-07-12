10b2c5f3c3c89569787a5035dfbfa1a52a33d2c8
aman0103 <aman0103@student.monash.edu>
2023-07-12 15:16:03 +1000

    Added comments to hangman.py in day-07

--- day-07/01-hangman.py
+++ day-07/01-hangman.py
@@ -66,32 +66,37 @@ hangman_ascii = ['''
 # Constants
 lives = 6
 correct_word_count = 0
-random_words = '''Hello bobby This is Abhishek'''
+random_words = ['ardvark','melbourne','london','camel','lion']
 
-word_selected = random.choice(random_words.lower().split())
+# Choosing a word randomly
+word_selected = random.choice(random_words)
 print(
     f"The word is {len(word_selected)} letter long as shown below. Trying guessing it\n")
 print("_\t"*len(word_selected))
 
+# Creating a list of _ spaces
 new_word = ("_\t"*len(word_selected)).strip('\t').split('\t')
 
 
 while lives != 0:
-    user_input = input("\nGuess a letter: ")
+    user_input = input("\nGuess a letter: ").lower()
 
+    # If the user input is invalid, throw error
     if len(user_input) == 0 or len(user_input) > 1:
         print("Please enter some valid letter to proceed with the game")
         continue
-
+    
+    # If the user input cannot be found in the selected word, reduce one life and continue
     if user_input not in word_selected:
         print(
             f"Bad choice!! I cannot see the letter in the word. You just lost one life and only {lives-1} lives remain")
         print(hangman_ascii[abs(lives - 7)])
         lives -= 1
         continue
-
+    
     print("That's right!!! I can see the letter in the word")
 
+    # Replacing the underscore with the value
     for index, letter in enumerate(word_selected):
         if user_input == letter:
             correct_word_count += 1
@@ -99,6 +104,9 @@ while lives != 0:
             new_word.insert(index, user_input)
 
     if len(word_selected) == correct_word_count:
+        print("Hard luck...You lose")
         lives = 0
 
     print("\n"+"\t".join(new_word))
+    print("\nYou win")
+
